6d709b21b02597cb0f9e3f04e105bd30c6dd5151
rudalstj <110973127+mxinseo@users.noreply.github.com>
2023-04-04 22:47:15 +0900

    Feat: 3주차 과제 및 실습 코드

---
+++ 경민서/hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository { //기능 4가지 구현
+    Member save(Member member); //회원이 저장소에 저장됨
+    Optional<Member> findById(Long id); // (Java8) Null 처리: Optional 감싸기
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); //지금까지 받은 모든 회원의 정보 반환
+}
