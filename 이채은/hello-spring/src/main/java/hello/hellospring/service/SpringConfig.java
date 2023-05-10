547ebf6659a8fe2ee804bdf7318d0a72f5508661
chan-nie <126068623+chan-nie@users.noreply.github.com>
2023-05-10 18:38:55 +0900

    Revert "Feat: 5주차 실습코드"
    This reverts commit e3596a29fe3a39387baca5f323e3f649314b285a.

--- 이채은/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ 이채은/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -26,12 +26,17 @@ public class SpringConfig {
 ***/
 @Configuration
 public class SpringConfig {
-    private final MemberRepository memberRepository;
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository);
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
